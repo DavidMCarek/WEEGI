fe5930677cd41e1c234cda6fe5eb07c0b2ed7c8d
sun shyne <sun shyne>
2017-08-31 18:13:40 -0400

    moved some shit added the bluetooth icon to be used

--- Android/app/src/main/java/com/sd1/weegy/MainActivity.java
+++ Android/app/src/main/java/com/sd1/weegy/MainActivity.java
@@ -7,19 +7,19 @@ import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner sourceSpinner;
-    private static final String[]sources = {"Live From Cyton", "SD Card"};
+   /* private Spinner sourceSpinner;
+    private static final String[]sources = {"Live From Cyton", "SD Card"};*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //sets up drop down to chose a source
+    /*//sets up drop down to chose a source
     sourceSpinner = (Spinner)findViewById(R.id.SourceSpinner);
         ArrayAdapter<String>sourceStrAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, sources);
         sourceStrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sourceSpinner.setAdapter(sourceStrAdapter);
-        //sourceSpinner.setOnItemSelectedListener(this);
+        //sourceSpinner.setOnItemSelectedListener(this);*/
     }
 }
