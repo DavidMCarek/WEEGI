c814a56dde02a68b83ec856de061150867ede029
davidmcarek <dmcarek@yahoo.com>
2017-09-21 19:39:16 -0400

    we have a drawer

--- Android/app/src/main/java/com/sd1/weegi/MainActivity.java
+++ Android/app/src/main/java/com/sd1/weegi/MainActivity.java
@@ -1,13 +1,21 @@
 package com.sd1.weegi;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -16,10 +24,18 @@ import com.sd1.weegi.fragments.ScannerFragment;
 
 import javax.inject.Inject;
 
-public class MainActivity extends Activity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
 
     @Inject
     RxBleClient mRxBleClient;
+
+    ActionBarDrawerToggle mDrawerToggle;
+
+    @BindView(R.id.navigation_drawer)
+    DrawerLayout mDrawerLayout;
 
     private static final int REQUEST_PERMISSIONS = 10;
     private static final int REQUEST_ENABLE_BT = 20;
@@ -37,8 +53,37 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerToggle.setDrawerIndicatorEnabled(false);
+        mDrawerToggle.setHomeAsUpIndicator(R.drawable.ic_menu);
+        mDrawerToggle.setToolbarNavigationClickListener(v -> {
+            if (mDrawerLayout.isDrawerVisible(GravityCompat.START)) {
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
 
         requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
