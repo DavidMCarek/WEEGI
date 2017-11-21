1ba48fcd0254e77172cef3f1e49cb07320975bd4
davidmcarek <dmcarek@yahoo.com>
2017-11-20 19:15:37 -0500

    fixed more stuff

--- Android/app/src/main/java/com/sd1/weegi/MainApplication.java
+++ Android/app/src/main/java/com/sd1/weegi/MainApplication.java
@@ -2,13 +2,6 @@ package com.sd1.weegi;
 
 import android.app.Application;
 
-import com.sd1.weegi.dagger.AppModule;
-import com.sd1.weegi.dagger.DaggerRxBleClientComponent;
-
-/**
- * Created by DMCar on 9/12/2017.
- */
-
 public class MainApplication extends Application {
 
     @Override
